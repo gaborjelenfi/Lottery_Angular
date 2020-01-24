7d41cfed2cf3effd78d9c0d00e112c3420d19b2b
Gabor Jelenfi <dzsub4@gmail.com>
2020-01-24 21:44:05 +0100

    updated the control validations

--- src/app/control/control.component.ts
+++ src/app/control/control.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild, DoCheck } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
 @Component({
@@ -6,12 +6,38 @@ import { NgForm } from '@angular/forms';
   templateUrl: './control.component.html',
   styleUrls: ['./control.component.css']
 })
-export class ControlComponent implements OnInit {
+export class ControlComponent implements OnInit, DoCheck {
   @ViewChild('f', {static: false}) numbersForm: NgForm;
+  fieldNum: number;
+  yourNum: number;
+  couponNum: number;
+  isfieldNumValid = false;
+  isyourNumValid = false;
+  iscouponNumValid = false;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  ngDoCheck() {
+    if (this.fieldNum <= 4 || this.fieldNum >= 100) {
+      this.isfieldNumValid = false;
+    } else {
+      this.isfieldNumValid = true;
+    }
+
+    if (this.yourNum <= 0 || this.yourNum >= 91) {
+      this.isyourNumValid = false;
+    } else {
+      this.isyourNumValid = true;
+    }
+
+    if (this.couponNum <= 0 || this.couponNum >= 1001) {
+      this.iscouponNumValid = false;
+    } else {
+      this.iscouponNumValid = true;
+    }
   }
 
   onSubmit() {
