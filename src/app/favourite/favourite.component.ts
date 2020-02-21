13a9d7020057f589d1146b44a1325d3d0b2d41ab
Gabor Jelenfi <dzsub4@gmail.com>
2020-02-21 21:30:09 +0100

    added animations to the favourites template

--- src/app/favourite/favourite.component.ts
+++ src/app/favourite/favourite.component.ts
@@ -3,11 +3,32 @@ import { ConnectionService } from '../connection.service';
 import { faStar } from '@fortawesome/free-solid-svg-icons';
 import { faTrash } from '@fortawesome/free-solid-svg-icons';
 import { Fav } from '../fav';
+import { trigger, transition, style, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-favourite',
   templateUrl: './favourite.component.html',
-  styleUrls: ['./favourite.component.css']
+  styleUrls: ['./favourite.component.css'],
+  animations: [
+    trigger('selected', [
+      transition(':enter', [
+        style({
+          opacity: 0,
+          transform: 'translateX(-200px)'
+        }),
+        animate('300ms', style({
+          transform: 'translateX(0)',
+          opacity: 1
+        }))
+      ]),
+      transition(':leave', [
+        animate('300ms ease-in', style({
+          transform: 'translateX(200px)',
+          opacity: 0
+        }))
+      ])
+    ])
+  ]
 })
 export class FavouriteComponent implements OnInit {
   faStar = faStar;
